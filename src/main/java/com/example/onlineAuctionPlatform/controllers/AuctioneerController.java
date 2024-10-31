c0d5bb8416628bb6c31ef15035c8ab2a14c66bed
nghia64582 <nghia64582@gmail.com>
2024-10-31 14:40:55 +0700

    update notes

--- src/main/java/com/example/onlineAuctionPlatform/controllers/AuctioneerController.java
+++ src/main/java/com/example/onlineAuctionPlatform/controllers/AuctioneerController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.onlineAuctionPlatform.entities.Auctioneer;
-import com.example.onlineAuctionPlatform.entities.Bidder;
 import com.example.onlineAuctionPlatform.entities.Product;
 import com.example.onlineAuctionPlatform.helpers.ProductHelper;
 import com.example.onlineAuctionPlatform.services.auctioneer.AuctioneerService;
